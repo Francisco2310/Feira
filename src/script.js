0ce4fa1c1fda29b954ce315cf1e7a26d082941e3
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2025-11-22 17:41:10 -0300

    responsivity added

--- src/script.js
+++ src/script.js
@@ -2,6 +2,31 @@ import { tsParticles } from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
 
 const config = {
+    responsive: [
+      {
+        maxWidth: 768,
+        options: {
+          particles: {
+            number: {
+              value: 30
+            },
+
+            links: {
+              distance: 100
+            }
+
+          },
+          interactivity: {
+            enable: false,
+            events: {
+              onHover: {
+                enable: false
+              }
+            }
+          }
+      }
+    }
+    ],
     autoplay: true,
     fullscreen: {
       enable: false,
