0edab8a280ae4af9ccb3548a12feef01ed042c62
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2025-11-22 17:04:36 -0300

    projeto criado

---
+++ src/script.js
@@ -0,0 +1,70 @@
+import { tsParticles } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const config = {
+    autoplay: true,
+    fullscreen: {
+      enable: false,
+      zIndex: -1
+    },
+    background: {
+        color: {
+            value: "#0f1a27"
+        }},
+    interactivity: {
+      modes: {
+        grab: {
+          distance: 400,
+        }
+      },
+      events: {
+        onHover: {
+          enable: true,
+          mode: "grab"
+      }}
+    },
+    particles: {
+        links: {
+          enable: true,
+          distance: 150,
+        },
+        number: { 
+          value: 50
+        },
+        color: {
+          value: "#ffffff"
+        },
+        shape: {
+          type:'circle'},
+        size: {
+            value: { max: 5, min: 1 }
+        },
+        move: {
+            enable: true,
+            speed: 2
+        },
+        opacity: {
+          value: 1
+
+        }
+    
+}};
+
+
+async function main() {
+    await loadSlim(tsParticles);
+    await tsParticles.load({
+      id: "tsparticles",
+      options: config,
+  })
+    .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+      }
+const particles = tsParticles.domItem(0);
+
+main();
+
