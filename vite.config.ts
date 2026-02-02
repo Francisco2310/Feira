07f8852855b883081fd71704fbc6dccfd8833475
Francisco2310 <74802078+Francisco2310@users.noreply.github.com>
2026-02-02 17:52:05 -0300

    optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(),viteSingleFile()
   ],
 })
